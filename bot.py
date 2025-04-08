1f6fc02cab94730c76224424e4721f6b534dc034
tigerhhlive <hossein.had@outlook.com>
2025-04-08 20:55:53 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -31,9 +31,7 @@ MIN_PERCENT_RISK = 0.05
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# -------------------------------
 # ارسال پیام به تلگرام فقط در صورت وجود سیگنال
-# -------------------------------
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
@@ -44,9 +42,7 @@ def send_telegram_message(message):
     except Exception as e:
         logging.error(f"Exception در ارسال پیام تلگرام: {e}")
 
-# -------------------------------
 # دریافت داده و اندیکاتورها
-# -------------------------------
 def get_data(timeframe, symbol):
     url = "https://min-api.cryptocompare.com/data/v2/histominute"
     aggregate = 5 if timeframe == '5m' else 15
